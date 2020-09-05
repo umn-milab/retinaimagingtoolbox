51b5d1bfebc9b11af3ab7afb7f2380d7f3cf404e
ivanalabounkova <62276029+ivanalabounkova@users.noreply.github.com>
2020-09-05 12:40:19 -0500

    Add files via upload

---
+++ rit_bss.m
@@ -0,0 +1,112 @@
+function [PCA_coeff, PCA_com_image, ICA_com_image, ICA_coeff, explained_cumulative] = ...
+    rit_bss(image_sequence, ncomponents, varargin)
+%SPATIALBSS estimates spatial blind source separation of an aligned video recording (i.e. 3D array)
+%   via principal component analysis and independent component analysis
+%
+%   [PCA_coeff, PCA_com_image, ICA_com_image, ICA_coeff, explained_cumulative] = ...
+%       SPATIALBSS(image_sequence, ncomponents, varargin)
+%       estimates ncomponets spatial principal components PCA_com_image and their timecourses PCA_coeff,
+%       ncomponents spatial independent components ICA_com_image and their timecourses ICA_coeff
+%       and percentils of cumulative explained data variability explained_cumulative. 
+%
+%   REQUIRED INPUTS:
+%   image_sequence must be a 3D array of type double where 1st and 2nd dimensions are space
+%   and the 3rd dimension is time.
+%
+%   ncomponents is a positive integer <= size(image_sequence,3)
+%
+%   OPTIONAL INPUTS:
+%   SPATIALBSS(image_sequence, ncomponents, HPf) where HPf is a two value row vector defining
+%   parameters of a high-pass filter of a timecourse of each pixel. First
+%   vector value is the video temporal sampling frequency (i.e. frame rate or fps).
+%   Second vector value is the cut-off frequency in Hz units.
+%   Default value is set to [0,0] which means the high-pass filtering is off.
+%
+%   SPATIALBSS(image_sequence, ncomponents, HPf, Mask) where Mask is a
+%   logical matrix or black & white image of type double defining region
+%   of interest where the blind source separation is estimated.
+%   Default value is set to the whole field of view:
+%
+%   SPATIALBSS(image_sequence, ncomponents, HPf, Mask, N) where N is a binary
+%   operator of value 0 or 1 deciding whether timecourse normalization of each 
+%   pixel is (value 1) or is not (value 0) normalized to mean = 0 and standard 
+%   deviation = 1.
+%   If high-pass filtering is also turned on, then the high pass filtering
+%   precedes the normalization.
+%   Default value is set to 0.
+%
+%   EXAMPLE:
+%   [PCA_coeff, PCA_com_image, ICA_com_image, ICA_coeff, explained_cumulative] = ...
+%       spatialbss(image_sequence, 4, [25 0.667],Mask,1);
+%
+%   COMPULSORY TOOLBOX:
+%   To run the fastica command, the fastica algorithm is needed to download and 
+%   setup in MATLAB session, the toolbox is publicly available at:
+%   https://research.ics.aalto.fi/ica/fastica/
+%
+%   CITE AS:
+%   Labounkova I, Labounek R, Nestrasil I, Odstrcilik J, Tornow R P,
+%   Kolar R, "Blind Source Separation of Phase Shifted Retinal Pulsatile
+%   Patterns in Optic Disc from Video-ophthalmoscopic Recordings," IEEE
+%   Transactions on Medical Imaging, 2020. [Under Review]
+%
+%   If you use the fastica algorithm, please CITE ALSO:
+%   Hyvarinen A and Oja E, “A Fast Fixed-Point Alogrithm for Independent
+%   Component Analysis,” Neural Computation, vol. 9, pp. 1483–1492, 1997.
+%
+%   Copyright 2020 Ivana Labounkova(1,2), Rene Labounek(2), Igor Nestrasil(2,3),
+%       Jan Odstrcilik(1), Ralf P. Tornow(4), Radim Kolar(1)
+%   (1) Department of Biomedical Engineering, Brno University of Technology, Brno, Czech Republic
+%   (2) Division of Clinical Behavioral Neuroscience, Department of Pediatrics, University of Minnesota, Minneapolis, USA
+%   (3) Center for Magnetic Resonance Research, Department of Radiology, University of Minnesota, Minneapolis, USA
+%   (4) DEpartment of Ophthalmology, Friedrich-Alexander University of Erlangen-Nuremberg, Erlangen, Germany
+
+%%
+   [xdim,ydim,frames] = size(image_sequence);
+   defaultMask = ones(xdim,ydim);
+   defaultN = 0;
+   defaultHPf = [0,0];
+   
+   p = inputParser;
+   addRequired(p,'image_sequence', @(x) isnumeric(x)); %&& size(x,1)>1 && size(x,2)>1);
+   addRequired(p,'ncomponents', @(x) isnumeric(x) && isscalar(x));
+   addOptional(p,'HPf', defaultHPf, @(x) isnumeric(x) && length(x)==2);% && isscalar(x) && (x > 0 && x <= 1));
+   addOptional(p,'Mask', defaultMask, @(x) size(x,1)>1 && size(x,2)>1 && sum(double(x(:)))>0 && sum(ismember(unique(double(x(:)))', [0,1]))>0 ); %isequal(unique(double(x(:)))', [0,1])
+   addOptional(p, 'N', defaultN, @(x) isnumeric(x) && ismember(x, [0,1]));
+   parse(p, image_sequence, ncomponents ,varargin{:});
+   clear image_sequence
+   %%
+    fps = p.Results.HPf(1);
+    fm = p.Results.HPf(2);
+    BW3D = reshape((repmat(p.Results.Mask,1,1,frames)),[], frames);
+    PCA_com_image = zeros(xdim,ydim,frames);  
+    %%
+    IS_T = reshape(p.Results.image_sequence,xdim*ydim,frames);
+    IS_T = reshape(IS_T(BW3D == 1),[],frames);  
+    if sum(p.Results.HPf)~=0 || p.Results.N~=0    
+        for px = 1:size(IS_T, 1)         
+            if sum(p.Results.HPf)~=0
+                Ns = ceil((fm(1)*frames)/fps);              
+                PX = fft(IS_T(px,:));
+                PX([(1:(Ns(1)-1)) ((end-Ns(1)+3):(end))]) = 0;
+                IS_T(px,:) = real(ifft((PX)));
+            end          
+            if p.Results.N == 1              
+                IS_T(px,:) = ( IS_T(px,:) - mean(IS_T(px,:)) ) / std(IS_T(px,:));
+            end
+        end
+    end   
+    %% spatial PCA
+    [PCA_coeff,score,~,~,explained,~] = pca(IS_T);
+    explained_cumulative = cumsum(explained);
+    PCA_com_image(BW3D == 1) = reshape(score,[],1);
+    coeff_reduced = PCA_coeff;
+    coeff_reduced(:,ncomponents+1:end) = 0;
+    PCA_com_image = PCA_com_image(:,:,1:ncomponents);
+    PCA_coeff = PCA_coeff(:,1:ncomponents);
+    %% spatial ICA
+    [icasig, ICA_coeff, ~] = fastica((score*coeff_reduced')','approach','symm');
+    ICA_com_image = zeros(xdim*ydim, size(icasig,1));
+    ICA_com_image(BW3D(:,1:(size(icasig,1)))==1) = reshape(icasig',[],1);
+    ICA_com_image = reshape(ICA_com_image, xdim, ydim, size(icasig,1));
+end
---
+++ rit_showmap.m
@@ -0,0 +1,111 @@
+function [output] = rit_showmap(background, overlay_image, treshold, varargin)
+%HELP
+
+
+%%
+   defaultAlpha = 0.9;
+   defaultMask = ones(size(background));
+   defaultScale = 'lin';
+   
+   expectedScale = {'lin','log'};
+   
+   p = inputParser;
+   
+   addRequired(p,'background', @(x) isnumeric(x) && size(x,1)>1 && size(x,2)>1);
+   addRequired(p,'overlay_image', @(x) isnumeric(x) && size(x,1)>1 && size(x,2)>1);
+   addRequired(p,'treshold', @(x) isnumeric(x) && isscalar(x));
+   
+   addOptional(p,'Alpha', defaultAlpha, @(x) isnumeric(x) && isscalar(x) && (x > 0 && x <= 1));
+   addOptional(p,'Mask', defaultMask, @(x) isnumeric(x) && size(x,1)>1 && size(x,2)>1 && isequal(unique(x), [0,1]));
+   
+   addParameter(p,'Scale',defaultScale,@(x) any(validatestring(x,expectedScale)));
+    
+   parse(p, background, overlay_image, treshold ,varargin{:});
+   
+   %%
+                
+                binarymask = zeros(size(p.Results.background));
+                binarymask(abs(p.Results.overlay_image)>p.Results.treshold & p.Results.Mask == 1) = 1;
+                
+                component = p.Results.overlay_image;
+                intmin = min(component(:));
+                intmax = max(component(:));
+                
+                component(abs(component)<p.Results.treshold) = 0;
+                
+%                 figure()
+                h_bar = colorbar('south', 'AxisLocation', 'out'); caxis([intmin, intmax])
+                orig_axis = get(h_bar, 'Ticks');
+                orig_axis = (sort(unique([orig_axis -p.Results.treshold p.Results.treshold])));
+                cla
+                
+                switch p.Results.Scale
+                    case 'lin'
+                        norm_component = uint8(((component-intmin)./(intmax-intmin))*255);
+                        
+%                         
+                        vls = orig_axis;
+                        vals1 = num2cell(vls);
+                        vals = (arrayfun(@(y) num2str(vals1{y}),1:length(vals1),'UniformOutput',false));                 
+                        
+                                                
+                    case 'log'
+                        component(component>0) = log(component(component>0));
+                        component(component<0) = -log(-component(component<0));
+                        component(component>log(intmax)) = log(intmax);
+                        component(component<-log(-intmin)) = -log(-intmin);
+                        norm_component = uint8(((component-(-log(-intmin)))./(log(intmax)-(-log(-intmin))))*255);
+                        
+                        vls1 = -log(abs(orig_axis(orig_axis<=-1)));
+                        vls2 = log(orig_axis(orig_axis>=1));
+                        
+                        val1 = num2cell((orig_axis(orig_axis<=-1)));
+                        val2 = num2cell(orig_axis(orig_axis>=1));
+                        
+                        
+                        if abs(p.Results.treshold) < 1 && abs(p.Results.treshold)>=0
+                            error('Tresh value is not applicable for log colorbar axis')
+                            return                   
+                        elseif p.Results.treshold == 1
+                            vls = [vls1(1:end-1), vls2];
+                            vals = [ (arrayfun(@(y) num2str(val1{y}),1:length(val1)-1,'UniformOutput',false)), {[num2str(-abs(p.Results.treshold)) '=' num2str(abs(p.Results.treshold))]}, ((arrayfun(@(y) num2str(val2{y}),2:length(val2),'UniformOutput',false)))];                 
+                        else
+                            vls = [vls1(1:end-1), vls2];
+                            vals = [ (arrayfun(@(y) num2str(val1{y}),1:length(val1)-1,'UniformOutput',false)), {'-1=1'}, ((arrayfun(@(y) num2str(val2{y}),2:length(val2),'UniformOutput',false)))];                 
+                        end                      
+                                                
+                end
+                 
+                norm_component = ind2rgb(norm_component, jet(256));             
+
+                rchannel = norm_component(:,:,1);
+                gchannel = norm_component(:,:,2);
+                bchannel = norm_component(:,:,3);
+
+                background_norm = (p.Results.background - min(p.Results.background(:)))./(max(p.Results.background(:))-min(p.Results.background(:)));
+                vizual_r = background_norm;
+                vizual_g = background_norm;
+                vizual_b = background_norm;
+
+                vizual_grey = cat(3, vizual_r, vizual_g, vizual_b);
+
+                vizual_r(logical(binarymask)) = rchannel(logical(binarymask));
+                vizual_g(logical(binarymask)) = gchannel(logical(binarymask));
+                vizual_b(logical(binarymask)) = bchannel(logical(binarymask));
+
+                vizual = cat(3, vizual_r, vizual_g, vizual_b);
+
+                component_data=component;
+                component_data = uint8(((component-intmin)./(intmax-intmin))*255);
+                
+                first_viz = imshow(vizual_grey, []);
+                hold on
+                viz_over = imshow(vizual, []);
+                set(viz_over, 'Alphadata', p.Results.Alpha)
+
+                component_transp = imshow(component_data); set(component_transp, 'Alphadata', 0.01); colormap jet;
+                
+                h_bar = colorbar('south', 'AxisLocation', 'out','Ticks',vls,'TickLabels',vals); abc = get(h_bar, 'Position'); set(h_bar, 'Position', [abc(1)-0.0 abc(2)-0.08 abc(3) abc(4)*5/6]); 
+                caxis([vls(1,1) vls(1,end)])                
+
+end
