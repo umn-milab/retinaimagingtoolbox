aedcaa99995228c3c4fbab6d17bdeb09da076920
Rene Labounek <rene.labounek@gmail.com>
2023-01-11 09:36:21 -0600

    update

--- rit_examplescript_denoise.m
+++ rit_examplescript_denoise.m
@@ -1,13 +1,13 @@
 clc;clear all;close all;
 
-% video_dir = '/home/range1-raid1/labounek/data-on-porto/retina_imaging/crop/sub-0001/ses-01';
-% video_name = 'sub-0001_ses-01_pos90_run1_06_01_2023_15_52_46_crop_1-661.mkv';
+video_dir = '/home/range1-raid1/labounek/data-on-porto/retina_imaging/crop/sub-0001/ses-01';
+video_name = 'sub-0001_ses-01_pos90_run1_06_01_2023_15_52_46_crop_1-661.mkv';
 
 % video_dir = '/home/range1-raid1/labounek/data-on-porto/retina_imaging/raw/sub-0001/ses-01';
 % video_name = 'sub-0001_ses-01_pos90_run1_06_01_2023_15_52_46.avi';
 
-video_dir = '/mnt/ganglium-raid1/data/ivanka/retina_imaging/crop/sub-0001/ses-01';
-video_name = 'sub-0001_ses-01_pos90_run1_06_01_2023_15_52_46_crop_1-661.mkv';
+% video_dir = '/mnt/ganglium-raid1/data/ivanka/retina_imaging/crop/sub-0001/ses-01';
+% video_name = 'sub-0001_ses-01_pos90_run1_06_01_2023_15_52_46_crop_1-661.mkv';
 
 ncomponents=25;
 visualization=1;
@@ -16,12 +16,12 @@ nframes = 661;
 video_file=fullfile(video_dir, video_name);
 v = VideoReader(video_file);
 
-for ind = 1:nframes
-    im = double(read(v,ind));
-    im = im(:,:,1);
-    
-    video(:,:,ind) = im;
-end
+% for ind = 1:nframes
+%     im = double(read(v,ind));
+%     im = im(:,:,1);
+%     
+%     video(:,:,ind) = im;
+% end
 
 [video, fps, nframes] = rit_videoload(video_file, 'gray');
 videoin = VideoReader(fullfile(video_dir, video_name));
