d87d4124c3a84ef2b2206b6298daf495bada8bde
Ivana Labounkova <labounkova.ivana@gmail.com>
2023-01-14 12:24:03 -0600

    comment deleted

--- rit_examplescript_denoise.m
+++ rit_examplescript_denoise.m
@@ -38,4 +38,3 @@ precision = 'double';
 video_file=fullfile(video_dir, video_name);
 [video, fps, nframes] = rit_videoload(video_file, 'gray');
 [video_denoised, denoise_stats] = rit_denoise(video,sratio,visualization,nframes,fps,save_dir,video_name,precision);
-% Hello world, Ivanka
