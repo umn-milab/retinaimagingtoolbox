639d765937eba1f6e24e12fc567bef7da11a38f5
Rene Labounek <rene.labounek@gmail.com>
2023-01-16 23:33:18 -0600

    Add mask drawing

---
+++ rit_draw_mask.m
@@ -0,0 +1,20 @@
+function mask = rit_draw_mask(im,fnamepath,ttle)
+%RIT_DRAW_MASK Summary of this function goes here
+%   Detailed explanation goes here
+
+    h.fig = figure(1);
+    set(h.fig,'Position',[150 150 1200 840]);
+    imshow(im, [] )
+    title(ttle,'Fontsize',12)
+%     positions = getPosition(imfreehand(gca));
+    positions = drawfreehand(gca);
+
+    % To get image from positions
+    mask = roipoly(size(im,1), size(im,2), positions.Position(:,1), positions.Position(:,2));
+    
+    % Save results
+    save([fnamepath '.mat'], 'positions','mask');
+    imwrite(mask,[fnamepath '.png'])
+    close(h.fig)
+end
+
