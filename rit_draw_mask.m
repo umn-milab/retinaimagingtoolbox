266745a820fbe6ea66cc5b10fbe101e0a91f1c20
ivanalabounkova <labounkova.ivana@gmail.com>
2026-02-09 21:18:37 -0600

    draw mask update

--- rit_draw_mask.m
+++ rit_draw_mask.m
@@ -6,12 +6,36 @@ function [mask, xy_positions] = rit_draw_mask(im,fnamepath,ttle)
     set(h.fig,'Position',[150 150 1200 840]);
     imshow(im, [] )
     title(ttle,'Fontsize',12)
+    mask = false(size(im));
+
+    currkey=0;
+    ind = 1;
+    % do not move on until enter key is pressed
+    while currkey~=1
+
+        positions{ind} = drawfreehand(gca);
+        xy_positions{ind} = [positions{ind}.Position(:,1) positions{ind}.Position(:,2)];
+        maskBW = roipoly(size(im,1), size(im,2), positions{ind}.Position(:,1), positions{ind}.Position(:,2));
+
+        mask = mask | maskBW;
+
+        pause; % wait for a keypress
+        currkey=get(gcf,'CurrentKey'); 
+        if strcmp(currkey, 'return') 
+            currkey=1 
+        else
+            currkey=0 
+            ind = ind+1;
+        end
+    end
+
+    
 %     positions = getPosition(imfreehand(gca));
-    positions = drawfreehand(gca);
 
     % To get image from positions
-    mask = roipoly(size(im,1), size(im,2), positions.Position(:,1), positions.Position(:,2));
-    xy_positions = [positions.Position(:,1) positions.Position(:,2)];
+    %mask = roipoly(size(im,1), size(im,2), positions.Position(:,1), positions.Position(:,2));
+
+    %xy_positions = [positions.Position(:,1) positions.Position(:,2)];
     
     % Save results
     save([fnamepath '.mat'], 'xy_positions','mask');
